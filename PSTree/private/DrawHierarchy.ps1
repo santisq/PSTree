a9c8b23342e3a51a1aad44a9eaec346cb2db1307
Santiago Squarzon <squarzon.santiago@gmail.com>
2021-12-14 15:43:05 -0300

    reverting to previous version

--- PSTree/private/DrawHierarchy.ps1
+++ PSTree/private/DrawHierarchy.ps1
@@ -4,52 +4,17 @@ param(
     [string]$PropertyName,
     [string]$RecursionProperty
 )
-function DrawHierarchy {
-param(
-    [System.Collections.ArrayList]$Array,
-    [string]$PropertyName,
-    [string]$RecursionProperty
-)
-
-    # Thanks https://github.com/ninmonkey for the Contribution :)
-    $bar = [char]::ConvertFromUtf32(0x2500)
-    $corner = [char]::ConvertFromUtf32(0x2514)
-    $pipe = [char]::ConvertFromUtf32(0x2502)
-    $connector = [char]::ConvertFromUtf32(0x251c)
-    
-    $cornerConnector = "${corner}$(${bar}*2) "
-
-    $Array | Group-Object $RecursionProperty |
-    Select-Object -Skip 1 | ForEach-Object {
-        $_.Group | ForEach-Object {
-            $_.$PropertyName = $_.$PropertyName -replace '\s{4}(?=\S)', $cornerConnector
-        }
-    }
-
-    for($i = 1; $i -lt $Array.Count; $i++)
-    {
-        $index = $Array[$i].$PropertyName.IndexOf($corner)
-        if($index -ge 0)
-        {
-            $z = $i - 1
-            while($Array[$z].$PropertyName[$index] -notmatch "$corner|\S")
-            {
-                $replace = $Array[$z].$PropertyName.ToCharArray()
-                $replace[$Index] = $pipe
-                $Array[$z].$PropertyName = -join $replace
-                $z--
-            }
-        
-            if($Array[$z].$PropertyName[$index] -eq $corner)
-            {
-                $replace = $Array[$z].$PropertyName.ToCharArray()
-                $replace[$Index] = $connector
-                $Array[$z].$PropertyName = -join $replace
-            }
-        }
-    }
-    $Array
-}
+    # Had to do this because of Windows PowerShell Default Encoding
+    # Not good at enconding stuff, probably a better way. Sorry for the ugliness :(
+    $bytes = @(
+        '226','148','148'
+        '44','226','148'
+        '128','44','226'
+        '148','130','44'
+        '226','148','156'
+    )
+    $corner, $horizontal, $pipe, $connector =
+    [System.Text.Encoding]::UTF8.GetString($bytes).Split(',')
     $cornerConnector = "${corner}$(${horizontal}*2) "
 
     $Array | Group-Object $RecursionProperty |
