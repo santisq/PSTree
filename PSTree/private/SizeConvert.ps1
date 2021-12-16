efe5e2111090e6ea614afda663ed64b9c4bb33a5
Santiago Squarzon <squarzon.santiago@gmail.com>
2021-12-16 00:39:02 -0300

    Update SizeConvert.ps1

--- PSTree/private/SizeConvert.ps1
+++ PSTree/private/SizeConvert.ps1
@@ -1,3 +1,5 @@
+# Inspired from https://stackoverflow.com/a/40887001/15339544
+
 function SizeConvert([double]$Size) {
     $suffix = "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"
     $index = 0
