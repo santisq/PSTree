f9600801a187cea4c80ef895a54defe1324672ab
Santiago Squarzon <squarzon.santiago@gmail.com>
2022-03-29 14:27:08 -0300

    Update PSTree.psm1

--- PSTree/PSTree.psm1
+++ PSTree/PSTree.psm1
@@ -2,9 +2,9 @@ $private = Join-Path $PSScriptRoot -ChildPath private
 $public = Join-Path $PSScriptRoot -ChildPath public
 
 Get-ChildItem -LiteralPath $private -Filter *.ps1 -Recurse |
-ForEach-Object { . $_.FullName }
+    ForEach-Object { . $_.FullName }
 
 Get-ChildItem -LiteralPath $public -Filter *.ps1 -Recurse |
-ForEach-Object { . $_.FullName }
+    ForEach-Object { . $_.FullName }
 
 Export-ModuleMember -Function Get-PSTree -Alias gpstree
