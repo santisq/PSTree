45b6f767baf744814b2a3ac99b4fa77b2b180e1b
Santiago Squarzon <squarzon.santiago@gmail.com>
2023-03-01 23:34:45 -0300

    i dont know what im doing :)

--- src/PSTree.cs
+++ src/PSTree.cs
@@ -9,8 +9,17 @@ internal static class PSTreeStatic
 {
     internal static string Indent(string inputString, int indentation)
     {
-        return new string(' ', 4 * indentation) + inputString;
+        return new string(' ', (4 * indentation) - 4) + "└── " + inputString;
     }
+
+    // internal static void DrawTree(object[] inputObject)
+    // {
+    //     char corner = '└';
+    //     char dash = '─';
+    //     char pipe = '│';
+    //     char lightVert = '├';
+    //     string cornerConnect = string.Concat(corner, dash, dash, ' ');
+    // }
 }
 
 internal sealed class PSTreeFile
@@ -42,9 +51,9 @@ internal sealed class PSTreeFile
 
 internal sealed class PSTreeDirectory
 {
-    internal readonly DirectoryInfo _instance;
+    internal DirectoryInfo Instance;
 
-    internal readonly int _depth;
+    internal int Depth;
 
     public string Hierarchy { get; internal set; }
 
@@ -54,30 +63,30 @@ internal sealed class PSTreeDirectory
 
     internal PSTreeDirectory(DirectoryInfo directoryInfo, int depth)
     {
-        _instance = directoryInfo;
-        _depth    = depth;
+        Instance  = directoryInfo;
+        Depth     = depth;
         Hierarchy = PSTreeStatic.Indent(directoryInfo.Name, depth);
         FullName  = directoryInfo.FullName;
     }
 
     public bool HasFlag(FileAttributes flag)
     {
-        return _instance.Attributes.HasFlag(flag);
+        return Instance.Attributes.HasFlag(flag);
     }
 
     public IEnumerable<FileInfo> EnumerateFiles() =>
-        _instance.EnumerateFiles();
+        Instance.EnumerateFiles();
 
     public IEnumerable<DirectoryInfo> EnumerateDirectories() =>
-        _instance.EnumerateDirectories();
+        Instance.EnumerateDirectories();
 
     public IEnumerable<FileSystemInfo> EnumerateFileSystemInfos() =>
-        _instance.EnumerateFileSystemInfos();
+        Instance.EnumerateFileSystemInfos();
 
     internal IEnumerable<string> GetParents(Dictionary<string, PSTreeDirectory> map)
     {
         int index = -1;
-        string path = _instance.FullName;
+        string path = Instance.FullName;
 
         while((index = path.IndexOf(Path.DirectorySeparatorChar, index + 1)) != -1)
         {
@@ -86,6 +95,135 @@ internal sealed class PSTreeDirectory
             if(map.ContainsKey(parent))
             {
                 yield return parent;
+            }
+        }
+    }
+}
+
+[CmdletBinding(DefaultParameterSetName = "Depth", PositionalBinding = false)]
+[Cmdlet(VerbsCommon.Get, "PSTree")]
+[Alias("pstree")]
+public sealed class PSTree : PSCmdlet
+{
+    private bool _isRecursive;
+
+    [Parameter(ValueFromPipeline = true, Position = 0)]
+    [Alias("PSPath")]
+    public string LiteralPath { get; set; } = null!;
+
+    [Parameter(ParameterSetName = "Depth")]
+    public int Depth = 3;
+
+    [Parameter(ParameterSetName = "Recurse")]
+    public SwitchParameter Recurse;
+
+    [Parameter]
+    public SwitchParameter Force;
+
+    [Parameter]
+    public SwitchParameter Directory;
+
+    [Parameter]
+    public SwitchParameter RecursiveSize;
+
+    protected override void BeginProcessing()
+    {
+        _isRecursive = RecursiveSize.IsPresent || Recurse.IsPresent;
+    }
+
+    protected override void ProcessRecord()
+    {
+        string absolutePath = GetUnresolvedProviderPathFromPSPath(LiteralPath);
+
+        try
+        {
+            if(!File.GetAttributes(LiteralPath).HasFlag(FileAttributes.Directory))
+            {
+                new PSTreeFile(new FileInfo(LiteralPath), 0);
+                return;
+            }
+        }
+        catch(Exception e)
+        {
+            ThrowTerminatingError(new ErrorRecord(e, "PSTree.GetAttributes", ErrorCategory.NotSpecified, absolutePath));
+        }
+
+        Dictionary<string, PSTreeDirectory> indexer = new();
+        Stack<PSTreeDirectory> stack = new();
+        List<PSTreeFile> files = new();
+        stack.Push(new PSTreeDirectory(new DirectoryInfo(absolutePath), 0));
+
+        while(stack.Count > 0)
+        {
+            PSTreeDirectory next = stack.Pop();
+            int level = next.Depth + 1;
+            long size  = 0;
+
+            try
+            {
+                IEnumerable<FileSystemInfo> enumerator = next.EnumerateFileSystemInfos();
+
+                bool keepProcessing = _isRecursive || level <= Depth;
+
+                foreach(FileSystemInfo item in enumerator)
+                {
+                    if(!Force.IsPresent && item.Attributes.HasFlag(FileAttributes.Hidden))
+                    {
+                        continue;
+                    }
+
+                    if(item is FileInfo file)
+                    {
+                        size += file.Length;
+
+                        if(Directory.IsPresent)
+                        {
+                            continue;
+                        }
+
+                        files.Add(new PSTreeFile(file, level));
+                        continue;
+                    }
+
+                    if(keepProcessing)
+                    {
+                        stack.Push(new PSTreeDirectory((DirectoryInfo) item, level));
+                    }
+                }
+
+                next.Length = size;
+                indexer[next.FullName] = next;
+
+                if(RecursiveSize.IsPresent)
+                {
+                    foreach(string parent in next.GetParents(indexer))
+                    {
+                        indexer[parent].Length += size;
+                    }
+                }
+
+                if(Recurse.IsPresent || next.Depth <= Depth)
+                {
+                    WriteObject(next);
+
+                    if(files.Count > 0 && (Recurse.IsPresent || level <= Depth))
+                    {
+                        WriteObject(files.ToArray(), true);
+                        files.Clear();
+                    }
+                }
+            }
+            catch(PipelineStoppedException)
+            {
+                throw;
+            }
+            catch(Exception e)
+            {
+                if(Recurse.IsPresent || next.Depth <= Depth) {
+                    WriteObject(next);
+                }
+
+                WriteError(new ErrorRecord(e, "PSTree.Enumerate", ErrorCategory.NotSpecified, next));
             }
         }
     }
