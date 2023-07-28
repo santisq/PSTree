4f4b459bcfde06db5115243dff3ca90ab842b965
Santiago Squarzon <squarzon.santiago@gmail.com>
2023-07-28 15:09:01 -0300

    few code changes and org. source code

--- src/PSTree/PSTreeExtensions.cs
+++ src/PSTree/PSTreeExtensions.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.IO;
-using System.Management.Automation;
-using System.Text.RegularExpressions;
-using Microsoft.PowerShell.Commands;
+﻿using System.Text.RegularExpressions;
 
 namespace PSTree;
 
-internal static class PSTreeStatic
+internal static class PSTreeExtensions
 {
-    private static readonly List<string> s_normalizedPaths = new();
-
     private static readonly Regex s_re = new(@"└|\S", RegexOptions.Compiled);
 
     internal static string Indent(this string inputString, int indentation) =>
@@ -51,76 +43,4 @@ internal static class PSTreeStatic
 
         return inputObject;
     }
-
-    internal static string[] NormalizePath(
-        this string[] paths,
-        bool isLiteral,
-        PSCmdlet cmdlet)
-    {
-        Collection<string> resolvedPaths;
-        ProviderInfo provider;
-        s_normalizedPaths.Clear();
-
-        foreach (string path in paths)
-        {
-            if (isLiteral)
-            {
-                string resolvedPath = cmdlet.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
-                    path, out provider, out _);
-
-                if (!provider.IsFileSystem())
-                {
-                    cmdlet.WriteError(ExceptionHelpers.InvalidProviderError(path, provider));
-                    continue;
-                }
-
-                if (!resolvedPath.Exists())
-                {
-                    cmdlet.WriteError(ExceptionHelpers.InvalidPathError(resolvedPath));
-                    continue;
-                }
-
-                s_normalizedPaths.Add(resolvedPath);
-                continue;
-            }
-
-            try
-            {
-                resolvedPaths = cmdlet.GetResolvedProviderPathFromPSPath(path, out provider);
-
-                foreach (string resolvedPath in resolvedPaths)
-                {
-                    if (!provider.IsFileSystem())
-                    {
-                        cmdlet.WriteError(ExceptionHelpers.InvalidProviderError(
-                            resolvedPath, provider));
-                        continue;
-                    }
-
-                    s_normalizedPaths.Add(resolvedPath);
-                }
-            }
-            catch (Exception e)
-            {
-                cmdlet.WriteError(ExceptionHelpers.ResolvePathError(path, e));
-            }
-        }
-
-        return s_normalizedPaths.ToArray();
-    }
-
-    internal static bool Exists(this string path) =>
-        File.Exists(path) || Directory.Exists(path);
-
-    internal static bool IsFileSystem(this ProviderInfo provider) =>
-        provider.ImplementingType == typeof(FileSystemProvider);
-
-    internal static bool IsArchive(this string path) =>
-        !File.GetAttributes(path).HasFlag(FileAttributes.Directory);
-
-    internal static bool IsDirectory(this string path) =>
-        File.GetAttributes(path).HasFlag(FileAttributes.Directory);
-
-    internal static bool IsHidden(this FileSystemInfo item) =>
-        item.Attributes.HasFlag(FileAttributes.Hidden);
 }
